eb267bd582e5571ea855130718d7414c85cc12d4
bakashigure <bakashigure@hotmail.com>
2022-12-13 11:58:36 +0000

    fix: cd

--- src/plugins/aquabot/utils.py
+++ src/plugins/aquabot/utils.py
@@ -211,6 +211,6 @@ def chat_cooldown_check(chat_cd, cd_id):
     # get current seconds
     now = time.time()
     cd = _config["chat_cooldown"]
-    if cd_id in chat_cd and now - chat_cd[id] < cd:
+    if cd_id in chat_cd and now - chat_cd[cd_id] < cd:
         return True
     return False
