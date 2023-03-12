8c6ad7f1a60a4c85f8c45e406cfb65f31962cf9f
bakashigure <bakashigure@hotmail.com>
2023-03-13 02:32:01 +0800

    doc: update doc

--- src/plugins/aquabot/text.py
+++ src/plugins/aquabot/text.py
@@ -139,10 +139,11 @@ text['chinese']['help_stats'] = f'''aqua stats\n\
 text["chinese"][
     "help_chat"
 ] = f"""aqua chat [语句]\n\
+缩写: ac [语句]\n\
 与 gpt-3.5-turbo-0301 对话 \n\
 支持上下文, 总token限制4095, 对话频率限制10s\n\
 你可以加bot好友后私聊使用, 避免群内刷屏\n\
-超过token上限后, 最开始的对话会被删除\n\
+超过token上限后, 会自动删除最开始的对话直到token足够\n\
 请不要摄正, 会被ban\n\
 """
 
